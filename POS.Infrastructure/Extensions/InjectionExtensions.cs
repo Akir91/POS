d5067d8079baca4bf0f99493ea5a54531e10ce70
Jesus Castañeda <jesus.castaneda@geninforule.com>
2023-02-13 13:37:27 -0600

    Configuracion de context

---
+++ POS.Infrastructure/Extensions/InjectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using POS.Infrastructure.Persistences.Contexts;
+
+namespace POS.Infrastructure.Extensions
+{
+    public static class InjectionExtensions
+    {
+        public static IServiceCollection AddInjectionInfraestructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var assembly = typeof(PosContext);
+
+        }
+    }
+}
